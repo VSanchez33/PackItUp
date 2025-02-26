b51bc72c2273f9e3d720ae24c9e0b8dc9fdd041b
Vincent Sanchez <vincentsanchez@vsanche18c43.nmsu.edu>
2025-02-26 15:16:47 -0700

    splash update

--- PackItUp/src/PackItUp/Main.java
+++ PackItUp/src/PackItUp/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application{
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("splash.fxml"));
         primaryStage.setTitle("Pack It Up");
-        primaryStage.setScene(new Scene(root, 640, 400));
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
