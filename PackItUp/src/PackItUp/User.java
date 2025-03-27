8bcc53f2643cc5cdb572d1840000ae0366ab1c4c
btyoung2 <btyoung@nmsu.edu>
2025-03-26 19:04:54 -0600

    User class (not completed)

---
+++ PackItUp/src/PackItUp/User.java
@@ -0,0 +1,20 @@
+/*
+ * Author:
+ *      Bryson Young
+ */
+
+package PackItUp;
+
+import java.util.ArrayList;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.collections.ObservableList;
+
+public class User {
+
+    private StringProperty userName;
+    private ObservableList<Box> boxList;
+    
+}
